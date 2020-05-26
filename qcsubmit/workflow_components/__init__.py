4de09ad3db4fbdbf188372db3a72822b3086cf7d
joshuahorton <joshy1691@gmail.com>
2020-05-26 15:43:40 +0100

    Added smarts substructure filter and tests.

--- qcsubmit/workflow_components/__init__.py
+++ qcsubmit/workflow_components/__init__.py
@@ -1,5 +1,11 @@
 from .base_component import BasicSettings, CustomWorkflowComponent, ToolkitValidator
 from .conformer_generation import StandardConformerGenerator
-from .filters import CoverageFilter, ElementFilter, MolecularWeightFilter, RotorFilter
+from .filters import (
+    CoverageFilter,
+    ElementFilter,
+    MolecularWeightFilter,
+    RotorFilter,
+    SmartsFilter,
+)
 from .fragmentation import WBOFragmenter
 from .state_enumeration import EnumerateStereoisomers, EnumerateTautomers
