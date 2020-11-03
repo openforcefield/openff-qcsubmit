8406dbb51f76021058f7e1867746469e20d34d72
jthorton <j.t.horton2@newcastle.ac.uk>
2020-11-03 15:16:21 +0000

    update pcm basis set.

--- qcsubmit/tests/test_submissions.py
+++ qcsubmit/tests/test_submissions.py
@@ -189,7 +189,7 @@ def test_basic_submissions_single_pcm_spec(fractal_compute_server):
     molecules = Molecule.from_file(get_data("butane_conformers.pdb"), "pdb")
 
     factory = BasicDatasetFactory(driver="energy")
-    factory.add_qc_spec(method="hf", basis="sto-2g", program=program, spec_name="default",
+    factory.add_qc_spec(method="hf", basis="sto-4g", program=program, spec_name="default",
                         spec_description="testing the single points with pcm",
                         implicit_solvent=PCMSettings(units="au", medium_Solvent="water"),
                         overwrite=True)
