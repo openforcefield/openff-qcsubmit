1e1cb3511c20ed8ca06860bcd3efca73aa680c02
Josh Horton <Josh.Horton@newcastle.ac.uk>
2021-01-18 12:24:01 +0000

    Geometric schema fix (#84)
    * update the geometric procedure schema, and change the reset default to True
    
    * remove unused import

--- qcsubmit/procedures.py
+++ qcsubmit/procedures.py
@@ -4,99 +4,26 @@ The procedure settings controllers
 
 from typing import Dict
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, Field
 from qcportal.models.common_models import OptimizationSpecification
+from typing_extensions import Literal
 
 
 class GeometricProcedure(BaseModel):
     """
     This is a settings class controlling the various runtime options that can be used when running geometric.
 
-    Attributes:
-        program: The name of the procedure.
-        coordsys: The name of the coordinate system which should be used during the optimisation.
-        enforce: The threshold (in a.u /rad) to activate precise constraint satisfaction.
-        epsilon: Small eigenvalue threshold.
-        reset: Reset the Hessian when the eigenvalues are under epsilon.
-        qccnv: Q-Chem style convergence criteria (i.e. gradient and either energy or displacement).
-        molcnv: Molpro style convergence criteria
-            (i.e. gradient and either energy or displacement, with different defaults).
-        check: The interval for checking the coordinate system for changes.
-        trust: Starting value of the trust radius.
-        tmax: Maximum value of trust radius.
-        maxiter: Maximum number of optimization cycles.
-        convergence_set: The set of convergence criteria to be used for the optimisation.
-    """
-
-    program: str = "geometric"
-    coordsys: str = "tric"
-    enforce: float = 0.0
-    epsilon: float = 1e-5
-    reset: bool = False
-    qccnv: bool = False
-    molcnv: bool = False
-    check: int = 0
-    trust: float = 0.1
-    tmax: float = 0.3
-    maxiter: int = 300
-    convergence_set: str = "GAU"
-    constraints: Dict = {}
-
-    class Config:
-        validate_assignment = True
-        title = "GeometricProcedure"
-
-    @validator("program")
-    def check_program(cls, program: str):
-        """
-        The program should not be changed from geometric.
-        """
-        if program.lower() != "geometric":
-            return "geometric"
-        else:
-            return program.lower()
-
-    @validator("coordsys")
-    def check_coordsys(cls, coordsys: str):
-        """
-        Make sure the user is assigning a valid geometric coordinate system.
-
-        Parameters:
-            coordsys: The coordinate system to be used during optimisation.
-
-        Raises:
-            ValueError: If the coordinate system is not supported by geometric.
-
-        Important:
-            The coordinate systems supported by geometric are:
+    Note:
+        The coordinate systems supported by geometric are:
 
             - `cart` Cartesian
             - `prim` Primitive a.k.a redundant
             - `dlc` Delocalised Internal Coordinates
             - `hdlc` Hybrid Delocalised Internal Coordinates
             - `tric` Translation-Rotation-Internal Coordinates, this is the default default
-        """
-        allowed_coordsys = ["cart", "prim", "dlc", "hdlc", "tric"]
-        if coordsys.lower() in allowed_coordsys:
-            return coordsys.lower()
-        else:
-            raise ValueError(
-                f"{coordsys} is not supported by geometric please pass a valid coordinate system."
-            )
 
-    @validator("convergence_set")
-    def check_convergence_set(cls, convergence: str):
-        """
-        Ensure a valid convergence set of criteria has been passed.
-
-        Parameters:
-            convergence: The convergence criteria set, see below for allowed values.
-
-        Raises:
-            ValueError: If the convergence set is not supported.
-
-        Important:
-            Geometric currently accepts the following convergence criteria sets:
+    Important:
+        Geometric currently accepts the following convergence criteria sets:
 
             | Set  | Set Name  | Energy  | GRMS  | GMAX  | DRMS  | DMAX  |
             |---|---|---|---|---|---|---|
@@ -107,23 +34,57 @@ class GeometricProcedure(BaseModel):
             | `INTERFRAG_TIGHT` | Interfrag tight | 1e-6 | 1e-5 | 1.5e-5 | 4.0e-4 | 6.0e-4 |
             | `GAU_TIGHT` | Gaussian tight | 1e-6 | 1e-5 | 1.5e-5 | 4e-5 | 6e-5 |
             | `GAU_VERYTIGHT` | Gaussian very tight | 1e-6 | 1e-6 | 2e-6 | 4e-6 | 6e-6 |
-        """
+    """
 
-        allowed_convergence = [
-            "GAU",
-            "NWCHEM_LOOSE",
-            "GAU_LOOSE",
-            "TURBOMOLE",
-            "INTERFRAG_TIGHT",
-            "GAU_TIGHT",
-            "GAU_VERYTIGHT",
-        ]
-        if convergence.upper() in allowed_convergence:
-            return convergence.upper()
-        else:
-            raise ValueError(
-                f"The requested convergence set {convergence} is not supported."
-            )
+    program: Literal["geometric"] = Field(
+        "geometric", description="The name of the program executing the procedure."
+    )
+    coordsys: Literal["tric", "prim", "dlc", "hdlc", "cart"] = Field(
+        "tric",
+        description="The type of coordinate system which should be used during the optimization. Choices are tric, prim, dlc, hdlc, and cart.",
+    )
+    enforce: float = Field(
+        0.0,
+        description="The threshold( in a.u / rad) to activate precise constraint satisfaction.",
+    )
+    epsilon: float = Field(1e-5, description="Small eigenvalue threshold.")
+    reset: bool = Field(
+        True, description="Reset the Hessian when the eigenvalues are under epsilon."
+    )
+    qccnv: bool = Field(
+        False,
+        description="Activate Q-Chem style convergence criteria(i.e.gradient and either energy or displacement).",
+    )
+    molcnv: bool = Field(
+        False,
+        description="Activate Molpro style convergence criteria (i.e.gradient and either energy or displacement, with different defaults).",
+    )
+    check: int = Field(
+        0, description="The interval for checking the coordinate system for changes."
+    )
+    trust: float = Field(0.1, description="Starting value of the trust radius.")
+    tmax: float = Field(0.3, description="Maximum value of trust radius.")
+    maxiter: int = Field(300, description="Maximum number of optimization cycles.")
+    convergence_set: Literal[
+        "GAU",
+        "NWCHEM_LOOSE",
+        "GAU_LOOSE",
+        "TURBOMOLE",
+        "INTERFRAG_TIGHT",
+        "GAU_TIGHT",
+        "GAU_VERYTIGHT",
+    ] = Field(
+        "GAU",
+        description="The set of convergence criteria to be used for the optimisation.",
+    )
+    constraints: Dict = Field(
+        {},
+        description="The list of constraints orginsed by set and freeze that should be used in the optimization",
+    )
+
+    class Config:
+        validate_assignment = True
+        title = "GeometricProcedure"
 
     def get_optimzation_spec(self) -> OptimizationSpecification:
         """
