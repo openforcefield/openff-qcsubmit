c0a899e27d726be7acbcdd12212ce50ec894ee8a
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-04-23 12:39:53 +0100

    Add SMARTS and SMILES result filters (#112)

--- openff/qcsubmit/results/filters.py
+++ openff/qcsubmit/results/filters.py
@@ -1,12 +1,13 @@
 import abc
 import logging
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, List, Optional, TypeVar
 
-from pydantic import BaseModel
+from openff.toolkit.topology import Molecule
+from pydantic import BaseModel, Field, root_validator
 
 if TYPE_CHECKING:
 
-    from openff.qcsubmit.results.results import _BaseResultCollection
+    from openff.qcsubmit.results.results import _BaseResult, _BaseResultCollection
 
     T = TypeVar("T", bound=_BaseResultCollection)
 
@@ -49,6 +50,12 @@ class ResultFilter(BaseModel, abc.ABC):
 
         filtered_collection = self._apply(result_collection.copy(deep=True))
 
+        filtered_collection.entries = {
+            address: entries
+            for address, entries in filtered_collection.entries.items()
+            if len(entries) > 0
+        }
+
         logger.info(
             f"{abs(filtered_collection.n_results - result_collection.n_results)} "
             f"results were removed after applying a {self.__class__.__name__} filter."
@@ -63,3 +70,133 @@ class ResultFilter(BaseModel, abc.ABC):
         filtered_collection.provenance["applied-filters"][filter_name] = {**self.dict()}
 
         return filtered_collection
+
+
+class SMILESFilter(ResultFilter):
+    """A filter which will remove or retain records which were computed for molecules
+    described by specific SMILES patterns.
+    """
+
+    smiles_to_include: Optional[List[str]] = Field(
+        None,
+        description="Only QC records computed for molecules whose SMILES representation "
+        "appears in this list will be retained. This option is mutually exclusive with "
+        "``smiles_to_exclude``.",
+    )
+    smiles_to_exclude: Optional[List[str]] = Field(
+        None,
+        description="Any QC records computed for molecules whose SMILES representation "
+        "appears in this list will be discarded. This option is mutually exclusive with "
+        "``smiles_to_include``.",
+    )
+
+    @root_validator
+    def _validate_mutually_exclusive(cls, values):
+
+        smiles_to_include = values.get("smiles_to_include")
+        smiles_to_exclude = values.get("smiles_to_exclude")
+
+        message = (
+            "exactly one of `smiles_to_include` and `smiles_to_exclude` must be "
+            "specified"
+        )
+
+        assert smiles_to_include is not None or smiles_to_exclude is not None, message
+        assert smiles_to_include is None or smiles_to_exclude is None, message
+
+        return values
+
+    @staticmethod
+    def _smiles_to_inchi_key(smiles: str) -> str:
+        return Molecule.from_smiles(smiles).to_inchikey(fixed_hydrogens=False)
+
+    def _apply(self, result_collection: "T") -> "T":
+
+        keys_to_include = (
+            None
+            if self.smiles_to_include is None
+            else {
+                self._smiles_to_inchi_key(smiles) for smiles in self.smiles_to_include
+            }
+        )
+        keys_to_exclude = (
+            None
+            if self.smiles_to_exclude is None
+            else {
+                self._smiles_to_inchi_key(smiles) for smiles in self.smiles_to_exclude
+            }
+        )
+
+        def _filter_function(entry: "_BaseResult") -> bool:
+
+            return (
+                entry.inchi_key in keys_to_include
+                if keys_to_include is not None
+                else entry.inchi_key not in keys_to_exclude
+            )
+
+        result_collection.entries = {
+            address: [*filter(_filter_function, entries)]
+            for address, entries in result_collection.entries.items()
+        }
+
+        return result_collection
+
+
+class SMARTSFilter(ResultFilter):
+    """A filter which will remove or retain records which were computed for molecules
+    which match specific SMARTS patterns.
+    """
+
+    smarts_to_include: Optional[List[str]] = Field(
+        None,
+        description="Only QC records computed for molecules that match one or more of "
+        "the SMARTS patterns in this list will be retained. This option is mutually "
+        "exclusive with ``smarts_to_exclude``.",
+    )
+    smarts_to_exclude: Optional[List[str]] = Field(
+        None,
+        description="Any QC records computed for molecules that match one or more of "
+        "the SMARTS patterns in this list will be discarded. This option is mutually "
+        "exclusive with ``smarts_to_include``.",
+    )
+
+    @root_validator
+    def _validate_mutually_exclusive(cls, values):
+
+        smarts_to_include = values.get("smarts_to_include")
+        smarts_to_exclude = values.get("smarts_to_exclude")
+
+        message = (
+            "exactly one of `smarts_to_include` and `smarts_to_exclude` must be "
+            "specified"
+        )
+
+        assert smarts_to_include is not None or smarts_to_exclude is not None, message
+        assert smarts_to_include is None or smarts_to_exclude is None, message
+
+        return values
+
+    def _apply(self, result_collection: "T") -> "T":
+        def _filter_function(entry: "_BaseResult") -> bool:
+
+            molecule: Molecule = Molecule.from_mapped_smiles(entry.cmiles)
+
+            if self.smarts_to_include is not None:
+
+                return any(
+                    len(molecule.chemical_environment_matches(smarts)) > 0
+                    for smarts in self.smarts_to_include
+                )
+
+            return all(
+                len(molecule.chemical_environment_matches(smarts)) == 0
+                for smarts in self.smarts_to_exclude
+            )
+
+        result_collection.entries = {
+            address: [*filter(_filter_function, entries)]
+            for address, entries in result_collection.entries.items()
+        }
+
+        return result_collection
--- openff/qcsubmit/tests/results/test_filters.py
+++ openff/qcsubmit/tests/results/test_filters.py
@@ -1,6 +1,13 @@
 import logging
 
-from openff.qcsubmit.results.filters import ResultFilter
+import pytest
+from pydantic import ValidationError
+
+from openff.qcsubmit.results.filters import (
+    ResultFilter,
+    SMARTSFilter,
+    SMILESFilter,
+)
 
 
 def test_apply_filter(basic_result_collection, caplog):
@@ -23,3 +30,49 @@ def test_apply_filter(basic_result_collection, caplog):
 
     assert "applied-filters" in filtered_collection.provenance
     assert "DummyFilter-0" in filtered_collection.provenance["applied-filters"]
+
+
+def test_smiles_filter_mutual_inputs():
+
+    with pytest.raises(ValidationError, match="exactly one of `smiles_to_include`"):
+        SMILESFilter(smiles_to_include=["C"], smiles_to_exclude=["CC"])
+
+
+def test_smarts_filter_mutual_inputs():
+
+    with pytest.raises(ValidationError, match="exactly one of `smarts_to_include`"):
+        SMARTSFilter(smarts_to_include=["C"], smarts_to_exclude=["CC"])
+
+
+@pytest.mark.parametrize(
+    "result_filter, expected_ids",
+    [
+        (SMILESFilter(smiles_to_include=["CCO"]), {"http://localhost:442": {"2", "3"}}),
+        (
+            SMILESFilter(smiles_to_exclude=["CCO"]),
+            {
+                "http://localhost:442": {"1", "4"},
+                "http://localhost:443": {"1", "2", "3", "4"},
+            },
+        ),
+        (
+            SMARTSFilter(smarts_to_include=["[#6]-[#8H1]"]),
+            {"http://localhost:442": {"1", "2", "3", "4"}},
+        ),
+        (
+            SMARTSFilter(smarts_to_exclude=["[#6]-[#8]"]),
+            {"http://localhost:443": {"1", "2", "3", "4"}},
+        ),
+    ],
+)
+def test_molecule_filter_apply(result_filter, expected_ids, basic_result_collection):
+
+    filtered_collection = result_filter.apply(basic_result_collection)
+
+    assert {*expected_ids} == {*filtered_collection.entries}
+
+    for address, entry_ids in expected_ids.items():
+
+        assert entry_ids == {
+            entry.record_id for entry in filtered_collection.entries[address]
+        }
