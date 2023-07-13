01e0a4ff222325293c46abb9576e29496ca04616
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-07-13 15:24:51 -0500

    Add `pre-commit`

--- openff/qcsubmit/tests/test_serializers.py
+++ openff/qcsubmit/tests/test_serializers.py
@@ -29,10 +29,13 @@ from openff.qcsubmit.testing import temp_directory
 from openff.qcsubmit.utils import get_data
 
 
-@pytest.mark.parametrize("serializer_type", [
-    pytest.param(("JSON", JsonSerializer), id="Json"),
-    pytest.param(("YAML", YamlSerializer), id="Yaml")
-])
+@pytest.mark.parametrize(
+    "serializer_type",
+    [
+        pytest.param(("JSON", JsonSerializer), id="Json"),
+        pytest.param(("YAML", YamlSerializer), id="Yaml"),
+    ],
+)
 def test_register_again(serializer_type):
     """
     Test adding another serializer
@@ -42,12 +45,15 @@ def test_register_again(serializer_type):
         register_serializer(*serializer_type)
 
 
-@pytest.mark.parametrize("compressor_type", [
-    pytest.param(("XZ", LZMACompressor), id="XZ"),
-    pytest.param(("BZ2", BZ2Compressor), id="BZ2"),
-    pytest.param(("GZ", GzipCompressor), id="GZ"),
-    pytest.param(("", NoneCompressor), id="Normal")
-])
+@pytest.mark.parametrize(
+    "compressor_type",
+    [
+        pytest.param(("XZ", LZMACompressor), id="XZ"),
+        pytest.param(("BZ2", BZ2Compressor), id="BZ2"),
+        pytest.param(("GZ", GzipCompressor), id="GZ"),
+        pytest.param(("", NoneCompressor), id="Normal"),
+    ],
+)
 def test_register_compressor_again(compressor_type):
     """
     Test adding a compressor again.
@@ -56,10 +62,13 @@ def test_register_compressor_again(compressor_type):
         register_compressor(*compressor_type)
 
 
-@pytest.mark.parametrize("deserializer_type", [
-    pytest.param(("JSON", JsonDeSerializer), id="Json"),
-    pytest.param(("YAML", YamlDeSerializer), id="Yaml")
-])
+@pytest.mark.parametrize(
+    "deserializer_type",
+    [
+        pytest.param(("JSON", JsonDeSerializer), id="Json"),
+        pytest.param(("YAML", YamlDeSerializer), id="Yaml"),
+    ],
+)
 def test_register_again_deserializer(deserializer_type):
     """
     Test registering a deserializer again.
@@ -126,10 +135,9 @@ def test_deserializer_error():
         deserialize("missing_file.json")
 
 
-@pytest.mark.parametrize("serializer", [
-    pytest.param(".json", id="Json"),
-    pytest.param(".yaml", id="yaml")
-])
+@pytest.mark.parametrize(
+    "serializer", [pytest.param(".json", id="Json"), pytest.param(".yaml", id="yaml")]
+)
 def test_serializer_round_trips(serializer):
     """
     Test serializing data to and from file with no compression.
@@ -144,16 +152,18 @@ def test_serializer_round_trips(serializer):
         assert data == deserialized_data
 
 
-@pytest.mark.parametrize("compression", [
-    pytest.param("xz", id="lzma"),
-    pytest.param("gz", id="gzip"),
-    pytest.param("", id="No compression"),
-    pytest.param("bz2", id="bz2")
-])
-@pytest.mark.parametrize("serialization", [
-    pytest.param("json", id="Json"),
-    pytest.param("yaml", id="yaml")
-])
+@pytest.mark.parametrize(
+    "compression",
+    [
+        pytest.param("xz", id="lzma"),
+        pytest.param("gz", id="gzip"),
+        pytest.param("", id="No compression"),
+        pytest.param("bz2", id="bz2"),
+    ],
+)
+@pytest.mark.parametrize(
+    "serialization", [pytest.param("json", id="Json"), pytest.param("yaml", id="yaml")]
+)
 def test_compression_serialization_round_trip_file_name(serialization, compression):
     """
     Test all of the different serialization and compression combinations.
@@ -161,7 +171,9 @@ def test_compression_serialization_round_trip_file_name(serialization, compressi
     """
     # get data in a dict format
     data = deserialize(get_data("settings_with_workflow.json"))
-    file_name = "".join(["settings_with_workflow", ".", serialization, ".", compression])
+    file_name = "".join(
+        ["settings_with_workflow", ".", serialization, ".", compression]
+    )
     # now export the file and read back
     with temp_directory():
         serialize(serializable=data, file_name=file_name, compression=None)
@@ -169,16 +181,18 @@ def test_compression_serialization_round_trip_file_name(serialization, compressi
         assert data == deserialized_data
 
 
-@pytest.mark.parametrize("compression", [
-    pytest.param("xz", id="lzma"),
-    pytest.param("gz", id="gzip"),
-    pytest.param("", id="No compression"),
-    pytest.param("bz2", id="bz2")
-])
-@pytest.mark.parametrize("serialization", [
-    pytest.param("json", id="Json"),
-    pytest.param("yaml", id="yaml")
-])
+@pytest.mark.parametrize(
+    "compression",
+    [
+        pytest.param("xz", id="lzma"),
+        pytest.param("gz", id="gzip"),
+        pytest.param("", id="No compression"),
+        pytest.param("bz2", id="bz2"),
+    ],
+)
+@pytest.mark.parametrize(
+    "serialization", [pytest.param("json", id="Json"), pytest.param("yaml", id="yaml")]
+)
 def test_compression_serialization_round_trip(serialization, compression):
     """
     Test all of the different serialization and compression combinations.
