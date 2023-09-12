c39b36e01d6bcea030464780ec0b9eb5b1a587d9
Jeffrey Wagner <jwagnerjpl@gmail.com>
2023-09-12 11:11:30 -0700

    manually set portal address

--- openff/qcsubmit/tests/conftest.py
+++ openff/qcsubmit/tests/conftest.py
@@ -10,7 +10,7 @@ from openff.qcsubmit.utils import get_data
 def public_client():
     """Setup a new connection to the public qcarchive client."""
 
-    return PortalClient()
+    return PortalClient("https://api.qcarchive.molssi.org:443/")
 
 
 @pytest.fixture(scope="function", autouse=True)
