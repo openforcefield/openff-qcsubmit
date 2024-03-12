97b991627e4959e565ad35ad2cb659898985008b
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-03-12 08:47:38 -0500

    Ensure `__all__` contains only str

--- openff/qcsubmit/utils/__init__.py
+++ openff/qcsubmit/utils/__init__.py
@@ -11,13 +11,13 @@ from openff.qcsubmit.utils.utils import (
 )
 
 __all__ = [
-    check_missing_stereo,
-    chunk_generator,
-    clean_strings,
-    condense_molecules,
-    get_data,
-    get_torsion,
-    remap_list,
-    get_symmetry_classes,
-    get_symmetry_group,
+    "check_missing_stereo",
+    "chunk_generator",
+    "clean_strings",
+    "condense_molecules",
+    "get_data",
+    "get_torsion",
+    "remap_list",
+    "get_symmetry_classes",
+    "get_symmetry_group",
 ]
