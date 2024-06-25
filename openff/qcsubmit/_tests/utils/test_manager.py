3d1aec74154f2f2341807b918906b6831cbefb35
Brent Westbrook <brentrwestbrook@gmail.com>
2024-06-25 12:51:53 -0400

    add no_internet context manager

--- openff/qcsubmit/_tests/utils/test_manager.py
+++ openff/qcsubmit/_tests/utils/test_manager.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
@@ -5,6 +6,25 @@ from qcportal import PortalClient
 
 from openff.qcsubmit.results import OptimizationResultCollection
 from openff.qcsubmit.utils.utils import get_data, portal_client_manager
+
+
+@contextmanager
+def no_internet():
+    """A context manager that temporarily disables access to the internet by
+    overriding `socket.socket` to raise an exception on use.
+    """
+    import socket
+
+    orig = socket.socket
+
+    def guard(*args, **kwargs):
+        raise Exception("socket accessed")
+
+    socket.socket = guard
+    try:
+        yield
+    finally:
+        socket.socket = orig
 
 
 def test_manager():
